d49e199c1e54d4f44742b0d2a6b3d538a27eeb6f
Daniel <golo7ov.danil@gmail.com>
2021-10-22 17:17:46 +0200

    17:17

--- loginSystem/urls.py
+++ loginSystem/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from loginSystem import views
 
 urlpatterns = [
-    path('', views.mainPage, name = 'mainPage'),
+    path('', views.mainPage, name="mainPage"),
     path('login/', views.loginPage, name="loginPage"),
     path('logout/', views.logoutUser, name="logoutPage"),
     path('register/', views.registerUser, name="registerPage")
